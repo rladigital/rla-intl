2d7d99444199e5d58ce852e0ac2b2c209d445e42
Tom Lawton <tom.lawton@rla.co.uk>
2017-10-26 15:04:49 +0100

    Removing unwanted span if using react 16+

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ function FormattedMessageFixed(props) {
 }
 
 export const trans = (message, values = {}, description = "") => {
+    //If React is past version 16 a string can be returned, avoiding an extra span
+    //The check involves using an undocumented property, so we check for it first
+    if (
+        typeof React.version === "string" &&
+        parseInt(React.version.split(".")[0], 10) >= 16
+    ) {
+        return (
+            <FormattedMessageFixed
+                id={message}
+                description={description}
+                defaultMessage={message}
+                values={values}
+            >
+                {message => message}
+            </FormattedMessageFixed>
+        );
+    }
     return (
         <FormattedMessageFixed
             id={message}
